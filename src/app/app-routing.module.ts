34701e3544a18834ca99d8ea87e92c38d36a4f65
Caio Cohen <caiocds1@gmail.com>
2023-09-18 09:30:43 -0300

    inicio da home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './main/home/home.component';
+import { RaizesComponent } from './main/raizes/raizes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'raizes', component: RaizesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './main/home/home.component';
+import { RaizesComponent } from './main/raizes/raizes.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    RaizesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
