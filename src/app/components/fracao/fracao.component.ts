53b4dadec33e9dae599775f90d3d863f9029d270
Caio Cohen <caiocds1@gmail.com>
2023-10-02 09:10:22 -0300

    newton-raphson

--- src/app/components/fracao/fracao.component.ts
+++ src/app/components/fracao/fracao.component.ts
@@ -8,10 +8,23 @@ import { Component, OnInit, Input } from '@angular/core';
 export class FracaoComponent implements OnInit {
   @Input() dividendo: string = "";
   @Input() divisor: string = "";
+  exponentialDividendo = false;
+  exponencialDividendo1:string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+    this.acharSupDividendo();
+  }
+
+  acharSupDividendo(){
+    const startIndex = this.dividendo.indexOf('<sup>');
+    const endIndex = this.dividendo.indexOf('</sup>');
+    if (startIndex !== -1 && endIndex !== -1 && startIndex < endIndex) {
+      this.exponencialDividendo1 = this.dividendo.substring(startIndex + 5, endIndex);
+      this.exponentialDividendo = true;
+      this.dividendo = this.dividendo.substring(0,startIndex)
+    }
   }
 
 }
