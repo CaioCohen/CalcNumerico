03d337fc34182eb6863463514bdea242440cfc6b
Caio Cohen <caio.cohen@vh.com.br>
2024-02-13 16:05:41 -0300

    insercao de videos e exemplos

--- src/app/main/valor-inicial/valor-inicial.component.ts
+++ src/app/main/valor-inicial/valor-inicial.component.ts
@@ -10,7 +10,7 @@ export class ValorInicialComponent implements OnInit {
   n2: number = 10;
   n3:number = 10;
   x1:number[] = [0]
-  y1:number[] = [8]
+  y1:number[] = [-64]
   x2:number[] = []
   y2:number[] = []
   x3:number[] = []
@@ -22,39 +22,69 @@ export class ValorInicialComponent implements OnInit {
   calculando2:boolean = true;
   calculando3:boolean = true;
 
+  selectedOption: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
-    this.recalcularEuler1();
-    this.analitico1();
-    this.calcularEulerMelhorado();
-    this.calcularRungeKutta();
+
   }
 
   flinha1(x:number){
-    return (4*(x-3)**3)/10
+    return 3*((x-4)**2)
   }
 
-  f(x:number){
-    return ((x-3)**4)/10
+  flinha2(x:number){
+    return 2*x
   }
 
-  analitico1(){
+  f1(x:number){
+    return (x-4)**3
+  }
+
+  f2(x:number){
+    return x**2
+  }
+
+  analitico(){
     let h = 8/1000
+    this.xA = []
+    this.yA = [];
     for(let i = 0; i < 1000;i++){
       this.xA.push(i*h);
-      this.yA.push(this.f(this.xA[i]))
+      if (this.selectedOption == 1){
+        this.yA.push(this.f1(this.xA[i]))        
+      }else{
+        this.yA.push(this.f2(this.xA[i]))
+      }
     }
   }
 
-  recalcularEuler1(){
+  selecionarExemplo(n: number){
+    this.selectedOption = +n;
+    if(this.selectedOption == 1){
+      this.y1 = [-64]
+    }else{
+      this.y1 = [0]
+    }
+    this.recalcularEuler();
+    this.calcularEulerMelhorado();
+    this.calcularRungeKutta();
+    this.analitico();
+  }
+
+  recalcularEuler(){
     this.calculando1 = true;
     let h = 8/this.n1;
     this.x1=[this.x1[0]]
     this.y1 = [this.y1[0]]
     for(let i = 1;i<this.n1;i++){
       this.x1.push(+(this.x1[0]+i*h).toFixed(2));
-      this.y1.push(+(this.y1[i-1]+h*this.flinha1(this.x1[i-1])).toFixed(2))
+      if (this.selectedOption == 1){
+        this.y1.push(+(this.y1[i-1]+h*this.flinha1(this.x1[i-1])).toFixed(2))
+      }else{        
+        this.y1.push(+(this.y1[i-1]+h*this.flinha2(this.x1[i-1])).toFixed(2))
+      }
     }
     setTimeout(() =>{this.calculando1 = false;},1)
   }
@@ -66,12 +96,17 @@ export class ValorInicialComponent implements OnInit {
     this.y2 = [this.y1[0]]
     for(let i = 1;i<this.n2;i++){
       this.x2.push(+(this.x2[0]+i*h).toFixed(2));
-      let k1 = h*this.flinha1(this.x2[i-1])
-      let k2 = h*this.flinha1(this.x2[i])
+      let k1 = 0;
+      let k2 = 0;
+      if (this.selectedOption == 1){
+        k1 = h*this.flinha1(this.x2[i-1])
+        k2 = h*this.flinha1(this.x2[i])
+      }else{
+        k1 = h*this.flinha2(this.x2[i-1])
+        k2 = h*this.flinha2(this.x2[i])
+      }
       this.y2.push(+(this.y2[i-1]+((k1+k2)/2)).toFixed(2))
     }
-    console.log(this.x2)
-    console.log(this.y2)
     setTimeout(() =>{this.calculando2 = false;},1)
   }
 
@@ -83,12 +118,23 @@ export class ValorInicialComponent implements OnInit {
     this.erro3 = [0]
     for(let i = 1;i<this.n3;i++){
       this.x3.push(+(this.x3[0]+i*h).toFixed(2));
-      let k1 = h*this.flinha1(this.x3[i-1])
-      let k2 = h*this.flinha1(this.x3[i-1]+(h/2))
-      let k3 = h*this.flinha1(this.x3[i-1]+(h/2))
-      let k4 = h*this.flinha1(this.x3[i])
+      let k1=0;
+      let k2=0;
+      let k3=0;
+      let k4=0;
+      if (this.selectedOption == 1){
+        k1 = h*this.flinha1(this.x3[i-1])
+        k2 = h*this.flinha1(this.x3[i-1]+(h/2))
+        k3 = h*this.flinha1(this.x3[i-1]+(h/2))
+        k4 = h*this.flinha1(this.x3[i])
+      }else{
+        k1 = h*this.flinha2(this.x3[i-1])
+        k2 = h*this.flinha2(this.x3[i-1]+(h/2))
+        k3 = h*this.flinha2(this.x3[i-1]+(h/2))
+        k4 = h*this.flinha2(this.x3[i])
+      }
       this.y3.push(+(this.y3[i-1]+(k1+2*k2+2*k3+k4)/6).toFixed(2))
-      this.erro3.push(this.y3[i]-this.f(this.x3[i]))
+      this.erro3.push(this.y3[i]-this.f1(this.x3[i]))
     }
     setTimeout(() =>{this.calculando3 = false;},1)
     console.log(this.erro3);
