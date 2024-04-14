cc358773741b9a3648da097423517f318432c4b4
Caio Cohen <caio.cohen@vh.com.br>
2024-04-14 18:23:16 -0300

    input nos simuladores

--- src/app/main/raizes/raizes.component.ts
+++ src/app/main/raizes/raizes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { evaluate, parse, compile } from 'mathjs';
 
 @Component({
   selector: 'app-raizes',
@@ -12,104 +13,115 @@ export class RaizesComponent implements OnInit {
   listaY: number[] = [];
   listaX2: number[] = [];
   listaY2: number[] = [];
-  ponto1: number[] = [-6,-216]
-  ponto2: number[] = [4,64]
+  ponto1: number[] = [-6, -216]
+  ponto2: number[] = [4, 64]
   inicio = -8;
   fim = 8;
   contadorSecante = 0;
+  a1Usuario: number = 0;
+  a2Usuario: number = 0;
+  b1Usuario: number = 0;
+  b2Usuario: number = 0;
+  funcaoUsuario: string = 'x^2';
+  result: number | null = null;
+  expr: any;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.recuperarCoordenadas();
   }
 
-  escolher(n:number){
+  escolher(n: number) {
+    this.listaX2 = [];
+    this.listaY2 = [];
+    this.salvar();
     this.simuladorEscolhido = n;
-    if(n == 2){
-      this.plotarRetaFalsaPosicao();
-      this.recarregarGrafico();
-    }else if (n == 3){
-      this.ponto2  = [];
-    }else if (n == 4){
-      this.contadorSecante = 0;
-      this.plotarRetaSecante();
-      this.recarregarGrafico();
+      if (n == 2) {
+        this.plotarRetaFalsaPosicao();
+        this.recarregarGrafico();
+      } else if (n == 3) {
+        this.ponto2 = [];
+        this.recarregarGrafico();
+      } else if (n == 4) {
+        this.contadorSecante = 0;
+        this.plotarRetaSecante();
+        this.recarregarGrafico();
+      }
+      else {
+        this.recarregarGrafico();
+      }
+
+  }
+
+  funcao(x: number) {
+    return this.expr.evaluate({ x: x })
+  }
+
+  derivada(expr: any, x: number, h: number = 0.001): number {
+    return (expr.evaluate({ x: x + h }) - expr.evaluate({ x: x })) / h;
+  }
+
+  recuperarCoordenadas() {
+    this.listaX = [];
+    this.listaY = [];
+    let h = (this.fim - this.inicio) / 1000;
+    for (let i = 0; i < 1000; i++) {
+      this.listaX.push(this.inicio + i * h);
+      this.listaY.push(this.funcao(this.listaX[i]))
     }
-    else{
-      this.listaX2 = [];
-      this.listaY2 = [];
-      this.recarregarGrafico();
-    }
   }
 
-  funcao(x: number){
-    return x**3
-  }
-
-  derivada(f: (x: number) => number, x: number, h: number = 0.001): number {
-    return (f(x + h) - f(x)) / h;
-  }
-
-  recuperarCoordenadas(){
-    let h = (this.fim - this.inicio)/1000;
-    for(let i = 0; i < 1000; i++){
-      this.listaX.push(this.inicio + i*h);
-      this.listaY.push(this.funcao(this.listaX[i]))        
-    }
-  }
-
-  metodoBissecao(){
+  metodoBissecao() {
     //se os sinais das extremidades são iguais
-    if((this.ponto1[1] > 0 && this.ponto2[1] > 0) || (this.ponto1[1] < 0 && this.ponto2[1] < 0)){
+    if ((this.ponto1[1] > 0 && this.ponto2[1] > 0) || (this.ponto1[1] < 0 && this.ponto2[1] < 0)) {
       alert("Os pontos a e b devem ter sinais diferentes");
       return;
     }
-    let medio = this.funcao((this.ponto1[0] + this.ponto2[0])/2);
+    let medio = this.funcao((this.ponto1[0] + this.ponto2[0]) / 2);
     //se o ponto medio e o ponto 1 possuem o mesmo sinal
-    if((medio < 0 && this.ponto1[1] < 0) || (medio > 0 && this.ponto1[1] > 0)){
-      this.ponto1 = [(this.ponto1[0] + this.ponto2[0])/2,medio];
-    }else{
-      this.ponto2 = [(this.ponto1[0] + this.ponto2[0])/2,medio];
+    if ((medio < 0 && this.ponto1[1] < 0) || (medio > 0 && this.ponto1[1] > 0)) {
+      this.ponto1 = [(this.ponto1[0] + this.ponto2[0]) / 2, medio];
+    } else {
+      this.ponto2 = [(this.ponto1[0] + this.ponto2[0]) / 2, medio];
     }
     this.recarregarGrafico();
   }
 
-  metodoFalsaPosicao(){
+  metodoFalsaPosicao() {
     //se os sinais das extremidades são iguais
-    if((this.ponto1[1] > 0 && this.ponto2[1] > 0) || (this.ponto1[1] < 0 && this.ponto2[1] < 0)){
+    if ((this.ponto1[1] > 0 && this.ponto2[1] > 0) || (this.ponto1[1] < 0 && this.ponto2[1] < 0)) {
       alert("Os pontos a e b devem ter sinais diferentes");
       return;
     }
-    let x0 = ((this.ponto1[0]*this.ponto2[1]) - (this.ponto2[0]*this.ponto1[1]))/(this.ponto2[1] - this.ponto1[1])
+    let x0 = ((this.ponto1[0] * this.ponto2[1]) - (this.ponto2[0] * this.ponto1[1])) / (this.ponto2[1] - this.ponto1[1])
     // se X0 e o ponto 1 continuam com sinais diferentes
-    if((this.funcao(x0) < 0 && this.ponto1[1] < 0) || (this.funcao(x0) > 0 && this.ponto1[1] > 0)){
-      this.ponto1 = [x0,this.funcao(x0)];
-    }else{
-      this.ponto2 = [x0,this.funcao(x0)];
+    if ((this.funcao(x0) < 0 && this.ponto1[1] < 0) || (this.funcao(x0) > 0 && this.ponto1[1] > 0)) {
+      this.ponto1 = [x0, this.funcao(x0)];
+    } else {
+      this.ponto2 = [x0, this.funcao(x0)];
     }
     this.plotarRetaFalsaPosicao();
     this.recarregarGrafico();
 
   }
 
-  metodoNewthon(){
+  metodoNewthon() {
     let xn = this.ponto1[0];
-    
-    xn = xn - (this.funcao(xn)/this.derivada(this.funcao,xn));
 
-    this.ponto1 = [xn,this.funcao(xn)];
+    xn = xn - (this.funcao(xn) / this.derivada(this.expr, xn));
+
+    this.ponto1 = [xn, this.funcao(xn)];
     this.recarregarGrafico();
 
   }
 
-  metodoSecante(){
+  metodoSecante() {
 
-    let x0 = ((this.ponto1[0]*this.ponto2[1]) - (this.ponto2[0]*this.ponto1[1]))/(this.ponto2[1] - this.ponto1[1])
-    if(this.contadorSecante % 2 == 0){
-      this.ponto1 = [x0,this.funcao(x0)]
-    }else{
-      this.ponto2 = [x0,this.funcao(x0)]
+    let x0 = ((this.ponto1[0] * this.ponto2[1]) - (this.ponto2[0] * this.ponto1[1])) / (this.ponto2[1] - this.ponto1[1])
+    if (this.contadorSecante % 2 == 0) {
+      this.ponto1 = [x0, this.funcao(x0)]
+    } else {
+      this.ponto2 = [x0, this.funcao(x0)]
     }
     this.contadorSecante++;
     this.plotarRetaSecante();
@@ -117,56 +129,71 @@ export class RaizesComponent implements OnInit {
 
   }
 
-  plotarRetaFalsaPosicao(){
+  plotarRetaFalsaPosicao() {
     this.listaX2 = [];
     this.listaY2 = [];
-    let h = (this.ponto2[0] - this.ponto1[0])/1000;
-      for(let i = 0; i < 1000; i++){
-        let x = this.ponto1[0] + i*h;
-        this.listaX2.push(x);
-        let y = this.ponto1[1] + (x-this.ponto1[0])*((this.ponto2[1]-this.ponto1[1])/(this.ponto2[0]-this.ponto1[0]))
-        this.listaY2.push(y)        
-      }
-  }
-
-  plotarRetaSecante(){
-    this.listaX2 = [];
-    this.listaY2 = [];
-    let h = (this.fim - this.inicio)/1000;
-      for(let i = 0; i < 1000; i++){
-        let x = this.ponto1[0] + i*h;
-        this.listaX2.push(x);
-        let y = this.ponto1[1] + (x-this.ponto1[0])*((this.ponto2[1]-this.ponto1[1])/(this.ponto2[0]-this.ponto1[0]))
-        this.listaY2.push(y)        
-      }
-  }
-
-  proximoPasso(){
-    switch(this.simuladorEscolhido){
-      case(1):
-        this.metodoBissecao();
-        break;
-      case(2):
-        this.metodoFalsaPosicao();
-        break;
-      case(3):
-        this.metodoNewthon();
-        break;
-      case(4):
-        this.metodoSecante();
-        break;
-      default:
-        //do nothing
+    let h = (this.ponto2[0] - this.ponto1[0]) / 1000;
+    for (let i = 0; i < 1000; i++) {
+      let x = this.ponto1[0] + i * h;
+      this.listaX2.push(x);
+      let y = this.ponto1[1] + (x - this.ponto1[0]) * ((this.ponto2[1] - this.ponto1[1]) / (this.ponto2[0] - this.ponto1[0]))
+      this.listaY2.push(y)
     }
   }
 
-  recarregarGrafico(){
+  plotarRetaSecante() {
+    this.listaX2 = [];
+    this.listaY2 = [];
+    let h = (this.fim - this.inicio) / 1000;
+    for (let i = 0; i < 1000; i++) {
+      let x = this.inicio + i * h;
+      this.listaX2.push(x);
+      let y = this.ponto1[1] + (x - this.ponto1[0]) * ((this.ponto2[1] - this.ponto1[1]) / (this.ponto2[0] - this.ponto1[0]))
+      this.listaY2.push(y)
+    }
+
+  }
+
+  proximoPasso() {
+    switch (this.simuladorEscolhido) {
+      case (1):
+        this.metodoBissecao();
+        break;
+      case (2):
+        this.metodoFalsaPosicao();
+        break;
+      case (3):
+        this.metodoNewthon();
+        break;
+      case (4):
+        this.metodoSecante();
+        break;
+      default:
+      //do nothing
+    }
+  }
+
+  recarregarGrafico() {
     let temp = this.simuladorEscolhido;
     this.simuladorEscolhido = 0;
-    setTimeout(() =>{
+    setTimeout(() => {
       this.simuladorEscolhido = temp;
-    },1)
+    }, 1)
 
+  }
+
+
+  salvar(): void {
+    try {
+      this.expr = parse(this.funcaoUsuario).compile();
+      this.ponto1 = [this.a1Usuario, this.a2Usuario];
+      this.ponto2 = [this.b1Usuario, this.b2Usuario];
+      this.recuperarCoordenadas();
+      this.simuladorEscolhido = 0;
+    } catch (error) {
+      console.error('Error parsing expression:', error);
+      alert('Invalid mathematical expression');
+    }
   }
 
 }
